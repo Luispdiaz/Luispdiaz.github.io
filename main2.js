7cb256c9baea67d6b69cc62feaedda255fc6278d
Luis Pernía <86423815+Luispdiaz@users.noreply.github.com>
2023-02-05 17:55:33 -0400

    final

--- main2.js
+++ main2.js
@@ -20,30 +20,6 @@ window.addEventListener('load',function(){
     }
     setInterval(Slide,6000);
  });
- 
-window.addEventListener('click',function(){
-    let img=[]
-    img[0]="imagenes/imagen1.jpg";
-    img[1]="imagenes/imagen2.jpg";
-    img[2]="imagenes/imagen3.jpg";
-    img[3]="imagenes/imagen4.jpg";
-    img[4]="imagenes/imagen5.jpg"
- 
-    let n=0;
- 
-    function Slide(){
-        document.slide.src = img[n];
- 
-        if(n<4){
-            n++;
-        }
-        else{
-            n=0;
-        }
-    }
-    setInterval(Slide,6000);
- });
-
 
 
 
