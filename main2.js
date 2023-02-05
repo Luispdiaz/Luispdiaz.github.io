1edc679d327d8b14cd34740bae23a50e9482aa42
Luis Pernía <86423815+Luispdiaz@users.noreply.github.com>
2023-02-05 18:06:09 -0400

    final

--- main2.js
+++ main2.js
@@ -1,18 +1,14 @@
-window.addEventListener("load", function() {
-    console.log("Entra")
+window.addEventListener('load',function(){
     let img=[]
     img[0]="imagenes/imagen1.jpg";
     img[1]="imagenes/imagen2.jpg";
     img[2]="imagenes/imagen3.jpg";
     img[3]="imagenes/imagen4.jpg";
     img[4]="imagenes/imagen5.jpg"
- 
     let n=0;
- 
     function Slide(){
-        document.slide.src = img[n];
- 
-        if(n<4){
+        document.imagen.src = img[n];
+        if(n<3){
             n++;
         }
         else{
@@ -20,7 +16,7 @@ window.addEventListener("load", function() {
         }
     }
     setInterval(Slide,1600);
- });
+ })
 
 
 
