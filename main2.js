4200fcc930440efb8ebc61493c4434f8bb6d8973
Luis Pernía <86423815+Luispdiaz@users.noreply.github.com>
2023-02-05 17:27:35 -0400

    Proyecto terminado

---
+++ main2.js
@@ -0,0 +1,24 @@
+window.addEventListener('load',function(){
+    let img=[]
+    img[0]="imagenes/imagen1.jpg";
+    img[1]="imagenes/imagen2.jpg";
+    img[2]="imagenes/imagen3.jpg";
+    img[3]="imagenes/imagen4.jpg";
+    img[4]="imagenes/imagen5.jpg"
+ 
+    let n=0;
+ 
+    function Slide(){
+        document.slide.src = img[n];
+ 
+        if(n<4){
+            n++;
+        }
+        else{
+            n=0;
+        }
+    }
+    setInterval(Slide,6000);
+ });
+
+
